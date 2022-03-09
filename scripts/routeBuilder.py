39c5bb3de89af9ed10a03697792db298355fc238
f1rA5h <shtey.alexey@gmail.com>
2022-03-09 13:49:41 +0300

    route

---
+++ scripts/routeBuilder.py
@@ -0,0 +1,38 @@
+routes = [[4, 3, 0], [2, 1, 0], [8, 7, 6, 0]]
+
+
+def make_route(start, end):
+    isFound = False
+    res = []
+    for i in range(len(routes)):
+        if isFound:
+            break
+        for j in range(len(routes[i])):
+            if routes[i][j] == start:
+                for k in range(len(routes[i])):
+                    if routes[i][k] == end:
+                        if (j < k):
+                            tmp = routes[i][j:k]
+                            for u in range(len(tmp)):
+                                res.append(tmp[u])
+                            res.append(routes[i][k])
+                        else:
+                            tmp = routes[i][k:j]
+                            res.append(routes[i][j])
+                            for u in range(len(tmp)):
+                                res.append(tmp[len(tmp) - u - 1])
+                        isFound = True
+    if not isFound:
+
+        tmp = make_route(start, 0)
+        for u in range(len(tmp)):
+            res.append(tmp[u])
+        res.remove(0)
+        tmp = make_route(0, end)
+        for u in range(len(tmp)):
+            res.append(tmp[u])
+
+    return res
+
+
+print(make_route(1, 6))
